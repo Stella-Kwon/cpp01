22b5108650f0f2080c499d8e24330efe0140c427
Sumin <kwontnals@gmail.com>
2024-12-06 17:24:56 +0200

    finished

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,29 +6,58 @@
 /*   By: skwon2 <skwon2@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 20:33:59 by skwon2            #+#    #+#             */
-/*   Updated: 2024/12/03 20:46:24 by skwon2           ###   ########.fr       */
+/*   Updated: 2024/12/06 14:29:24 by skwon2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <string>
+#include "replace.hpp"
 
-int main()
+std::string Replace::replaceStr(const std::string& line, const std::string& s1, const std::string& s2)
 {
-    std::string str = "HI THIS IS BRAIN";
-    std::string* ptr = &str;
-    std::string& ref = str;
-    
-    std::cout << "--------------------------------" << std::endl;
-    std::cout << "Address of \"str\" : " << &str << std::endl;
-    std::cout << "Address of \"strPTR\" pointed at: " << ptr << std::endl; //address held by stringPTR which in value
-    std::cout << "Address of \"strPTR\" : " << &ptr << std::endl; //address stringPTR itself
-    std::cout << "Address of \"strREF\" : " << &ref << std::endl;
-    std::cout << "--------------------------------" << std::endl;
-    std::cout << "Value of \"str\" : " << str << std::endl;
-    std::cout << "Value of \"strPTR\" pointed at: " << *ptr << std::endl; // value pointed to stringPTR
-    std::cout << "Value of \"strPTR\" : " << ptr << std::endl; // value pointed to stringPTR
-    std::cout << "Value of \"strREF\" : " << ref << std::endl;
-    std::cout << "--------------------------------" << std::endl;
+    std::string res = line;
+    size_t pos = 0;
+    while ((pos = line.find(s1, pos)) !=  std::string::npos)
+    {
+        res = res.substr(0, pos) + s2 + res.substr(pos + s1.length());
+        pos += s2.length();
+    }
+    return (res);
+}
+
+void Replace::replaceInFile(const std::string& filename, const std::string& s1, const std::string& s2)
+{
+    if (s1.empty())
+        return ;
+    // inputFile(filename) opens the file named filename for reading.
+    std::ifstream inputFile(filename);
+    if (!inputFile.is_open())
+    {
+        std::cout << "Inputfile:" << filename << " is not opened." << std::endl;
+        return;
+    }
+    std::ofstream outputFile(filename + ".replace");
+    if (!outputFile.is_open())
+    {
+        std::cout << "outputfile: " << filename << ".replace is not opened." << std::endl;
+        return;
+    }
+    std::string line;
+    while (std::getline(inputFile, line))
+    {
+        outputFile << replaceStr(line, s1, s2) << std::endl;
+    }
+    inputFile.close();
+    outputFile.close();
+}
+        
+
+int main(int argc, char **argv)
+{
+    if (argc != 4)
+    {
+        std::cout << "Usage : \"" << argv[0] << " filename s1 s1\" is the right syntax.\n";
+        return 1;
+    }
+    Replace::replaceInFile(argv[1], argv[2], argv[3]);
     return 0;
 }
--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,29 +6,31 @@
 /*   By: skwon2 <skwon2@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 20:33:59 by skwon2            #+#    #+#             */
-/*   Updated: 2024/12/03 20:46:24 by skwon2           ###   ########.fr       */
+/*   Updated: 2024/12/06 17:02:00 by skwon2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <string>
+#include "Harl.hpp"
 
-int main()
-{
-    std::string str = "HI THIS IS BRAIN";
-    std::string* ptr = &str;
-    std::string& ref = str;
-    
-    std::cout << "--------------------------------" << std::endl;
-    std::cout << "Address of \"str\" : " << &str << std::endl;
-    std::cout << "Address of \"strPTR\" pointed at: " << ptr << std::endl; //address held by stringPTR which in value
-    std::cout << "Address of \"strPTR\" : " << &ptr << std::endl; //address stringPTR itself
-    std::cout << "Address of \"strREF\" : " << &ref << std::endl;
-    std::cout << "--------------------------------" << std::endl;
-    std::cout << "Value of \"str\" : " << str << std::endl;
-    std::cout << "Value of \"strPTR\" pointed at: " << *ptr << std::endl; // value pointed to stringPTR
-    std::cout << "Value of \"strPTR\" : " << ptr << std::endl; // value pointed to stringPTR
-    std::cout << "Value of \"strREF\" : " << ref << std::endl;
-    std::cout << "--------------------------------" << std::endl;
+int main() {
+    Harl h;
+
+    std::cout << "Test DEBUG level:" << std::endl;
+    h.complain("DEBUG");
+
+    std::cout << "\nTest INFO level:" << std::endl;
+    h.complain("INFO");
+
+    std::cout << "\nTest WARNING level:" << std::endl;
+    h.complain("WARNING");
+
+    std::cout << "\nTest ERROR level:" << std::endl;
+    h.complain("ERROR");
+
+    std::cout << "\nTest an unknown level:" << std::endl;
+    h.complain("UNKNOWN");
+
+    std::cout << "\nTest an unknown level:" << std::endl;
+    h.complain("");
     return 0;
 }
