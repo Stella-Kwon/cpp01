360b7791ab57a11ce7cddf4932b28a1ecdf5719c
Sumin <kwontnals@gmail.com>
2024-12-06 17:24:21 +0200

    finished

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: skwon2 <skwon2@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 20:33:59 by skwon2            #+#    #+#             */
-/*   Updated: 2024/12/04 00:02:36 by skwon2           ###   ########.fr       */
+/*   Updated: 2024/12/04 14:15:00 by skwon2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main()
     club.setType("some other type of club");
     bob.attack();
 
-    Weapon club2 = Weapon("crude spiked club");
+    Weapon club2 = Weapon("crude spiked club2");
     HumanB jim("Jim"); 
     // giving "jim" like this is not std::string type, it is c-string literal, and a temporary object which cannot reference in non-const-reference.
     // so if you put datatype as HumanB(std::string& name); 
@@ -29,7 +29,7 @@ int main()
     //Giving const will allow temporary object to be referenced so that give const. 
     jim.setWeapon(club2);
     jim.attack();
-    club.setType("some other type of club");
+    club2.setType("some other type of club2");
     jim.attack();
     return 0;
 }
